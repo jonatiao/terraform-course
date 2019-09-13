9305f87954ab40bda2ac924800d2d0ba404e8ed1
jonatiao <jonatiao@gmail.com>
2019-09-13 14:15:45 -0300

    Update jenkins-init.sh

--- jenkins-packer-demo/scripts/jenkins-init.sh
+++ jenkins-packer-demo/scripts/jenkins-init.sh
@@ -8,13 +8,18 @@ if [ "`echo -n $DEVICE_FS`" == "" ] ; then
   # wait for the device to be attached
   DEVICENAME=`echo "${DEVICE}" | awk -F '/' '{print $3}'`
   DEVICEEXISTS=''
-  while [[ -z $DEVICEEXISTS ]]; do
+  #while [[ -z $DEVICEEXISTS ]]; do
+  #  echo "checking $DEVICENAME"
+  #  DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
+  #  if [[ $DEVICEEXISTS != "1" ]]; then
+  #    sleep 15
+  #  fi
+  #done
+  while [[ $DEVICEEXISTS != "1" ]]; do
     echo "checking $DEVICENAME"
     DEVICEEXISTS=`lsblk |grep "$DEVICENAME" |wc -l`
-    if [[ $DEVICEEXISTS != "1" ]]; then
-      sleep 15
-    fi
-  done
+    sleep 15
+  done  
   pvcreate ${DEVICE}
   vgcreate data ${DEVICE}
   lvcreate --name volume1 -l 100%FREE data
